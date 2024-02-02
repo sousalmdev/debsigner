cb8653304c1cc713092d550958b61d9b17c9f8cc
Leandro Sousa <151054241+sousalmdev@users.noreply.github.com>
2024-02-02 09:26:06 -0300

    Update mail.js

--- js/mail.js
+++ js/mail.js
@@ -1,12 +1,11 @@
 
 
 document.addEventListener('DOMContentLoaded', function(){
-    // Inicialização do emailjs
     emailjs.init('Eku1Fdhoqs81Po5C0');
 
-    // Obtenção dos elementos do formulário e do botão de envio
     const form = document.getElementById('meuFormulario');
     const submitButton = document.getElementById('submit');
+    
     function showLoad(){
         let loading = document.getElementById('backgroundOverlayLoading');
         loading.style.display = 'block';
@@ -14,34 +13,30 @@ document.addEventListener('DOMContentLoaded', function(){
             loading.style.display='none';
         },1000)
     }
-    // Função para mostrar o alerta e depois de 2 segundos ocultá-lo
+
     function mostrarAlerta() {  
-        
         let backgroundOverlay = document.getElementById('backgroundOverlay');
 
         setTimeout(function(){
         backgroundOverlay.style.display = 'block';
         },1000)
     
-
-        // Oculta o alerta após 2 segundos
         setTimeout(function() {
             backgroundOverlay.style.display = 'none';
-        }, 3000); // 2000 milissegundos = 2 segundos
+        }, 3000); 
     }
  
-    // Verificação da existência do formulário e do botão de envio
-    if (form && submitButton) {
-        // Adiciona um evento de envio ao formulário
-        form.addEventListener('submit', function(event) {
-            event.preventDefault(); // Impede o envio padrão do formulário
 
-            // Obtenção dos valores do formulário
+    if (form && submitButton) {
+
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
+
             const meuNome = 'Debsigner';
             const nome = document.getElementById('nome').value;
             const email = document.getElementById('email').value;
 
-            // Envio do email usando EmailJS
+
             emailjs.send('service_glr8yco', 'template_pztqkr8', {
                 to_name: nome,
                 to_email: email,
@@ -49,10 +44,10 @@ document.addEventListener('DOMContentLoaded', function(){
             }).then(function(response) {
                 console.log('Email enviado com sucesso!', response);
                 showLoad();
-                mostrarAlerta(); // Chama a função para mostrar o alerta
+                mostrarAlerta();
             }, function(error) {
                 console.error('Erro ao enviar email:', error);
-               mostrarAlertaNegativo();
+               alert('Erro! Tente novamente');
             });
         });
     } else {
