772afe1d7168b9da69674a071a9b014c0d25f4f9
sousalmdev <leandromedeiros690@gmail.com>
2024-03-19 19:10:11 -0300

    refactoring

--- js/mail.js
+++ js/mail.js
@@ -1,34 +1,10 @@
-
-
 document.addEventListener('DOMContentLoaded', function(){
     emailjs.init('Eku1Fdhoqs81Po5C0');
 
     const form = document.getElementById('meuFormulario');
     const submitButton = document.getElementById('submit');
-    
-    function showLoad(){
-        let loading = document.getElementById('backgroundOverlayLoading');
-        loading.style.display = 'block';
-        setTimeout(function(){
-            loading.style.display='none';
-        },1000)
-    }
-
-    function mostrarAlerta() {  
-        let backgroundOverlay = document.getElementById('backgroundOverlay');
-
-        setTimeout(function(){
-        backgroundOverlay.style.display = 'block';
-        },1000)
-    
-        setTimeout(function() {
-            backgroundOverlay.style.display = 'none';
-        }, 3000); 
-    }
- 
 
     if (form && submitButton) {
-
         form.addEventListener('submit', function(event) {
             event.preventDefault();
 
@@ -36,18 +12,31 @@ document.addEventListener('DOMContentLoaded', function(){
             const nome = document.getElementById('nome').value;
             const email = document.getElementById('email').value;
 
-
             emailjs.send('service_glr8yco', 'template_pztqkr8', {
                 to_name: nome,
                 to_email: email,
                 from_name: meuNome
             }).then(function(response) {
-                console.log('Email enviado com sucesso!', response);
-                showLoad();
-                mostrarAlerta();
+                console.log('Email enviado com sucesso!', response);  
+
+                Swal.fire({
+                    title: "Email Enviado!",
+                    text: "Aguarde alguns segundos!",
+                    icon: "success",
+                    iconColor:'#ff59ba',
+                    confirmButtonText: 'Valeu!',
+                    confirmButtonColor:'#ff59ba',
+                    color:'white'
+                       
+                });
+
             }, function(error) {
                 console.error('Erro ao enviar email:', error);
-               alert('Erro! Tente novamente');
+                Swal.fire({
+                    title: "Error!",
+                    text: "Erro ao enviar email. Por favor, tente novamente mais tarde.",
+                    icon: "error"
+                });
             });
         });
     } else {
