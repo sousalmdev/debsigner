17afce5b774b7b34810e5f22f484aab4f3824a6c
sousalmdev <leandromedeiros690@gmail.com>
2024-04-30 23:48:09 -0300

    Fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="scroll-smooth">
+    <html  lang="en" className="scroll-smooth">
       <ChakraProvider>
         {" "}
         <Header />
