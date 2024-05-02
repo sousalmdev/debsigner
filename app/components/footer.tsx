3f515ad3d37e5e11e70186cc1670504c5221eec9
sousalmdev <leandromedeiros690@gmail.com>
2024-05-02 13:59:55 -0300

    Add: Provisory Privacy Policy

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -18,7 +18,7 @@ export const Footer = () => {
             </li>
             <li className="font-normal sm:text-base text-sm">
               Políticas de Privacidade <br />
-              <Link className="font-bold underline" href={"/"}>
+              <Link className="font-bold underline" href={"https://www.gov.br/mdh/pt-br/acesso-a-informacao/politica-de-privacidade#:~:text=A%20LGPD%20pro%C3%ADbe%20a%20pr%C3%A1tica,exerc%C3%ADcio%20dos%20direitos%20do%20titular.&text=As%20organiza%C3%A7%C3%B5es%20devem%20adotar%20medidas,n%C3%A3o%20autorizados%2C%20perdas%20e%20danos.&text=Assim%20como%20direitos%2C%20voc%C3%AA%20tamb%C3%A9m,utilizar%20um%20servi%C3%A7o%20do%20MDHC."}>
                 Clique aqui
               </Link>
             </li>
